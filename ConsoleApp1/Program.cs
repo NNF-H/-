dbe78436702dc92eeecc96e73eddfcdeab7ef4f0
MC-J <cjfdfgs.113@gmail.com>
2020-10-03 13:42:48 +0800

    zhouasn

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello发的457453 World!");
+        }
+    }
+}
